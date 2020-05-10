c81da0f84d7e01097f4164cf3a8abd3e7d608d9e
Jim Petas <gdpetas@hotmail.com>
2020-05-11 00:33:49 +0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,54 @@
+using Epiker.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace epiker
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        //We can acceess appsettings values from the configuration
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+        }
+
+
+        //Midleware
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //If we are in the dev mode then we will se a development fliendly exception page
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Redirect us to the https address 
+            app.UseHttpsRedirection();
+
+            //Get us to the controller
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
