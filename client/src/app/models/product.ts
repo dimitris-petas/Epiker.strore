483f192fc23d19ff5c008ec4db7548554aac2873
Dimitris Petas <petasjim@gmail.com>
2020-09-16 22:57:40 +0300

    client removed

--- client/src/app/models/product.ts
+++
@@ -1,9 +0,0 @@
-export interface IProduct {
-    id: number;
-    name: string;
-    description: string;
-    pictureUrl: string;
-    price: number;
-    productType: string;
-    productBrand: string;
-}
